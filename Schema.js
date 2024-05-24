1e739f52d2139b5969b8a4f23146b035b432f7d2
devashish <mshubh612@gmail.com>
2024-05-24 17:25:19 +0530

    express session and cookies

--- Schema.js
+++ Schema.js
@@ -5,8 +5,15 @@ module.exports.listingSchema = Joi.object({
         title: Joi.string().required(),
         description: Joi.string().required(),
         location: Joi.string().required(),
-        country: Joi.number().required().min(0),
-        price: Joi.string().required(),
-        image: Joi.string().allow("", null),
+        country: Joi.string().required(),
+        price: Joi.number().required(),
+        image: Joi.string().allow("", null).optional(),
     }).required(),
 });
+
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        comment: Joi.string().required(),
+    }).required(),
+}); 
--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/WrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema } = require("./Schema.js");
+const listings = require("./Routes/listing.js");
+const reviews = require("./Routes/review.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+
 
 main().catch((err) => console.log(err));
 
@@ -20,7 +22,12 @@ const validateListing = (req, res, next) => {
     } else{
         next();
     }
-}
+};
+
+
+
+
+
 
 async function main() {
  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
@@ -34,79 +41,33 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const sessionOptions = {
+    secret: "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true
+};
+
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
 
+app.use(session(sessionOptions));
+app.use(flash());
 
-//Index Route
-app.get("/listings",wrapAsync( async (req, res) => {
-    const allListings =  await Listing.find({});
-    res.render("listings/index.ejs", {allListings});
-}));
-
-//New Route
-app.get("/listings/new", (req, res) => {
-    res.render("listings/new.ejs");
-})
-
-
-//Show Route
-app.get("/listings/:id", wrapAsync(async (req, res) => {
-    let { id } = req.params; 
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing} );
-}));
-
-//Create Route
-app.post("/listings", validateListing,  wrapAsync(async (req, res, next) => {
-        const newListing = new Listing(req.body.listing);
-        await newListing.save();
-        res.redirect("/listings");
-})
-);
-
-//Edit Route
-app.get("/listings/:id/edit", wrapAsync(async (req,res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", {listing});
-}));
-
-//UPDATE Route
-app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {    
-    let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
-   
-    res.redirect(`/listings/${id}`);
-}))
-//ARE YOU THERE... yeah i m searching for the error on mygpt4 ...ok 
-//Delete Route
-app.delete("/listings/:id", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    let deleteListing = await Listing.findByIdAndDelete(id);
-    console.log(deleteListing);
-    res.redirect("/listings");
-}))
+app.use((req, res, next) => {
+    res. locals.success = req.flash("success");
+    next();
+});
 
 
 
-// app.get("/testListing", async (req, res) => {
-//     console.log("REQ AT TEST LISTING")
-//     let sampleListing = new Listing({
-//         title: "my new Villa",
-//         description: "By the beach",
-//         price: 1200,
-//         location: "Calangute, Goa",
-//         country: "India",
-//     });
 
-//     let demoListing = await sampleListing.save();
-//     console.log(demoListing)
 
-//     console.log("sample was saved");
-//     res.send("Successful Testing");
-// })
+app.use("/listings", listings);
+app.use("/listings/:id/reviews" , reviews);
+
+
+
 
 app.all("*", (req,res, next) => {
     next(new ExpressError(404, "page Not Found!"));
