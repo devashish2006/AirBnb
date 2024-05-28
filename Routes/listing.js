a61907be924871711cb4c8b9edf5465d9b011754
devashish <mshubh612@gmail.com>
2024-05-28 23:39:52 +0530

    added owner in listings and some authorization part in listing edit snd delete

--- Routes/listing.js
+++ Routes/listing.js
@@ -3,23 +3,12 @@ const express = require("express");
 const router = express.Router();
 
 const wrapAsync = require("../utils/WrapAsync.js");
-const { listingSchema, reviewSchema } = require("../Schema.js");
-const ExpressError = require("../utils/ExpressError.js");
 const listings = require("../Routes/listing.js");
 const Listing = require("../models/listing.js");
-const { isLoggedIn } = require("../middleware.js");
+const { isLoggedIn, isOwner,validateListing } = require("../middleware.js");
 
 
-const validateListing = (req, res, next) => {
-    let {error} = listingSchema.validate(req.body);
 
-    if (error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    } else{
-        next();
-    }
-};
 
 
 
@@ -38,7 +27,7 @@ router.get("/new", isLoggedIn, (req, res) => {
 //Show Route
 router.get("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params; 
-    const listing = await Listing.findById(id).populate("reviews");
+    const listing = await Listing.findById(id).populate("reviews").populate("owner");
     if(!listing) {
         
     req.flash("error", "Listing you requested for does not exist!");
@@ -49,7 +38,8 @@ router.get("/:id", wrapAsync(async (req, res) => {
 
 //Create Route
 router.post("/", validateListing, isLoggedIn,  wrapAsync(async (req, res, next) => {
-        const newListing = new Listing(req.body.listing);
+        const newListing = new Listing(req.body.listing); 
+        newListing.owner = req.user._id;
         await newListing.save();
         req.flash("success", "New Listing Created!");
         res.redirect("/listings");
@@ -57,7 +47,7 @@ router.post("/", validateListing, isLoggedIn,  wrapAsync(async (req, res, next) 
 );
 
 //Edit Route
-router.get("/:id/edit",isLoggedIn, wrapAsync(async (req,res) => {
+router.get("/:id/edit",isLoggedIn, isOwner, wrapAsync(async (req,res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     if(!listing) {
@@ -69,7 +59,7 @@ router.get("/:id/edit",isLoggedIn, wrapAsync(async (req,res) => {
 }));
 
 //UPDATE Route
-router.put("/:id", validateListing, isLoggedIn, wrapAsync(async (req, res) => {    
+router.put("/:id", isLoggedIn, isOwner, validateListing, wrapAsync(async (req, res) => {    
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
 
@@ -79,7 +69,7 @@ router.put("/:id", validateListing, isLoggedIn, wrapAsync(async (req, res) => {
 }))
 
 //Delete Route
-router.delete("/:id", isLoggedIn,  wrapAsync(async (req, res) => {
+router.delete("/:id", isLoggedIn, isOwner,  wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deleteListing = await Listing.findByIdAndDelete(id);
     console.log(deleteListing);
--- middleware.js
+++ middleware.js
@@ -1,3 +1,8 @@
+const Listing = require("./models/listing");
+
+const { listingSchema, reviewSchema } = require("./Schema.js");
+const ExpressError = require("./utils/ExpressError.js");
+
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()) {
         req.flash("error", "you must be logged in to create listing!");
@@ -5,3 +10,36 @@ module.exports.isLoggedIn = (req, res, next) => {
     }
     next();
 }
+
+
+module.exports.isOwner = async(req, res, next) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    if (!listing.owner.equals(res.locals.currUser._id)) {
+        req.flash("error", "You are not the owner of this listing");
+        return res.redirect(`/listings/${id}`);
+    }    
+    next();
+};
+
+module.exports.validateListing = (req, res, next) => {
+    let {error} = listingSchema.validate(req.body);
+
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else{
+        next();
+    }
+};
+
+module.exports.validateReview = (req, res, next) => {
+    let {error} = reviewSchema.validate(req.body);
+
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else{
+        next();
+    }
+};  
